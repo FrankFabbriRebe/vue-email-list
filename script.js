c45c1ce59c7e86704bc11139c823ed4aaa8b0505
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-12-14 11:07:35 +0100

    feat: use API

--- script.js
+++ script.js
@@ -16,6 +16,12 @@ createApp ({
 
         for(let i = 0; i < 10; i++) {
             
+            // API to generete random email
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // console.log(response.data.response);
+            })
+
         }
 
     }
